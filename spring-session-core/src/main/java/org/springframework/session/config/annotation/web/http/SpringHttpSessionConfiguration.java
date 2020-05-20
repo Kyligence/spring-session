7c16a1c1538324fd8b132a5a199a4bfbcfb43614
qiangsheng.ye <qiangsheng.ye@kyligence.io>
2020-05-20 17:56:12 +0800

    KE-13728 change kylin config to spring.session.timeout

--- spring-session-core/src/main/java/org/springframework/session/config/annotation/web/http/SpringHttpSessionConfiguration.java
+++ spring-session-core/src/main/java/org/springframework/session/config/annotation/web/http/SpringHttpSessionConfiguration.java
@@ -110,7 +110,7 @@ public class SpringHttpSessionConfiguration implements InitializingBean, Applica
 	@Value("${kylin.web.session-skip-header-name:Auto}")
 	private String skipUpdateSessionHeaderName;
 
-	@Value("${kylin.web.session-timeout:-1}")
+	@Value("${spring.session.timeout:-1}")
 	private int sessionTimeout;
 
 	public static boolean secureRandomCreateEnabled;
