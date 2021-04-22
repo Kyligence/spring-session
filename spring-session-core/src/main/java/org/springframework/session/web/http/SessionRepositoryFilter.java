390ec4f92bcf6c62909e30f86599c364cd1b7723
qiangsheng.ye <qiangsheng.ye@kyligence.io>
2021-04-22 09:57:28 +0800

    KE-25654 save session when delay half timeout.

--- spring-session-core/src/main/java/org/springframework/session/web/http/SessionRepositoryFilter.java
+++ spring-session-core/src/main/java/org/springframework/session/web/http/SessionRepositoryFilter.java
@@ -244,7 +244,11 @@ public class SessionRepositoryFilter<S extends Session> extends OncePerRequestFi
 				String requestedSessionId = getRequestedSessionId();
 				clearRequestedSessionCache();
 				try {
-					SessionRepositoryFilter.this.sessionRepository.save(session);
+					S sourceSession = SessionRepositoryFilter.this.sessionRepository.findById(session.getId());
+					if ( null == sourceSession || (System.currentTimeMillis() -
+							sourceSession.getLastAccessedTime().toEpochMilli()) > session.getMaxInactiveInterval().toMillis() * 500 ) {
+						SessionRepositoryFilter.this.sessionRepository.save(session);
+					}
 				} catch (Exception e) {
 					throw new SaveSessionException("Failed to save session!", e);
 				}
